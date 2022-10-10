5b3aae5bb08879f3d2db03d357c843d119860e0e
vermadatla97 <vermadatla97@gmail.com>
2022-10-10 01:51:42 -0600

    Final Changes

--- process.c
+++ process.c
@@ -4,7 +4,6 @@ Name: Blake Barton
 Email: blake.barton@okstate.edu
 Date: 9/25/22
 Description:
-
 This file currently has these primary functions:
 1 - take user input on which input file and column (temporary)
 2 - read the input file and save them into an array [readFile()]
@@ -12,13 +11,10 @@ This file currently has these primary functions:
 4 - create parallel processes - one for each unique value in column [processCreation()]
 5 - send row data to each process based on its unique value [processCreation()]
 6 - closes parallel processes [processCreation()] (will likely need to be changed for the menu options)
-
 compile: gcc process.c -lrt
 execute: ./a.out
-
 tested all options succesfully in csx2
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,6 +28,8 @@ tested all options succesfully in csx2
 #include <errno.h>
 #include <mqueue.h>
 
+#include "process.h"
+
 #define QUEUE_NAME "/Queue-3-test"
 #define PERMISSIONS 0660
 #define MAX_MESSAGES 5
@@ -42,8 +40,6 @@ typedef char* String;
 
 char bookInfo[703][6][210];
 char amazonBestsellers[550][7][210];
-
-int processCreation(int processes, int location, char* values[], int sizes[], int rows, int cols);
 
 /* readFile()
 desc: reads the input file and parses data and stores in either bookInfo[][][] or amazonBestsellers[][][] based on file
@@ -112,7 +108,7 @@ then it calls processCreation() to create the new processes and communicate via 
 input: # of rows, # of columns, user specified column, # of unique processes
 output: 0
 */
-int processSetup(int rows, int columns, int col, int processes) {
+int processSetup(int rows, int columns, int col, int processes, int *fout, int *fin) {
 
     //selects file array to read from
     char (*array)[columns][210];
@@ -151,18 +147,21 @@ int processSetup(int rows, int columns, int col, int processes) {
         }
         flag = 0; //clears flag for next row
     }
+    
     //prints unique categories and number of items associated (TEMP)
-    for (int k = 0; k < processes; k++) {
-        printf("%s %d\n", category[k], amount[k]);
-
+    /*for (int k = 0; k < processes; k++) {
+        printf("%s %dTEST\n", category[k], amount[k]);
     }
+    */
 
-    int test = processCreation(processes, col, category, amount, rows, columns);
+    printf("processes running...\n");
+    processCreation(processes, col, category, amount, rows, columns, fout, fin);
 
 
     return 0;
 
 }
+
 
 /* processCreation()
 desc: creates child processes for each unique value within user's column
@@ -170,8 +169,8 @@ also sends row data from the parent to the corresponding child processes using P
 input: # of processes, user specified column, array of unique value names, array of occurence of each unique value, # of rows, # of columns
 output: 0
 */
-int processCreation(int processes, int location, char* values[], int sizes[], int rows, int cols)
-{
+int processCreation(int processes, int location, char* values[], int sizes[], int rows, int cols, int *fout, int *fin)
+{   
 
     char (*array)[cols][210];
     if (cols == 6) array = bookInfo;
@@ -181,8 +180,13 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
 
     pid_t pids[processes]; //stores all the child pids
 
-
     mqd_t server_qd, client_qd; //server queue descriptor
+
+    int fd[2];
+    if(pipe(fd) < 0){
+        perror("pipe");
+        exit(1);
+    }
 
     struct mq_attr attr = {
         .mq_flags = 0,
@@ -200,6 +204,7 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
         }
 
         else if (pids[i] == 0) { //child
+            close(fd[0]);
 
             //array for storing contents of rows
             char list[sizes[i]][cols][210];
@@ -237,6 +242,7 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
                     exit(1);
                 }
 
+
                 //adds to matrix unless closing message
                 if (strcmp(in_buffer, "quit") != 0) {
                     sprintf(list[row][col], "%s", in_buffer);
@@ -252,23 +258,46 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
 
                     }
                 }
+                
             }
+            
 
+            /*
             //test for printing: REMOVE
             for (int a = 0; a < row; a++) {
                 for (int b = 0; b < cols; b++) {
-                    printf("%s %d %d %s\n", client_name, a, b, list[a][b]);
+                    printf("Client: %s A: %d B: %d List: %s\n", client_name, a, b, list[a][b]);
                 }
+            }*/
+            
+            
+            char proc[350];
+            for(int r = 0; r < sizes[i]; r++){
+                sleep(0.01);
+                memset(proc,0,strlen(proc));
+                for(int c = 0; c < cols-1; c++){
+                    //if(c == location)
+                    //    continue;
+                    strcat(proc, list[r][c]);
+                    strcat(proc, ",");
+                }
+                strcat(proc, list[r][cols-1]);
+                size_t length = strlen(proc);
+                write(fd[1], proc, length);
             }
+            sleep(0.05);
+            char done[5] = "done";
+            size_t length = strlen(done);
+            write(fd[1], done, length);
 
-            printf("%s closing\n", client_name);
+            //printf("%s closing\n", client_name);
             if (mq_close(client_qd) == -1) {
                     perror("Child: mq_close\n");
                     exit(1);
                 }
 
             if (mq_unlink(client_name) == -1) {
-                perror("Child: mq_unlinK\n");
+                perror("Child: mq_unlink\n");
                 exit(1);
             }
 
@@ -280,7 +309,7 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
     //PARENT PROCESS SECTION
 
     //creates the server (parent)
-    if (server_qd = mq_open(QUEUE_NAME, O_RDWR | O_CREAT | O_NONBLOCK, PERMISSIONS, &attr) == -1) {
+    if ((server_qd = mq_open(QUEUE_NAME, O_RDWR | O_CREAT | O_NONBLOCK, PERMISSIONS, &attr) == -1)) {
         perror("Server: mq_open server\n");
         exit(1);
     }
@@ -311,7 +340,8 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
                 //sends data from all columns to corresponding child process
                 for (int k = 0; k < cols; k++) {
                     char out_buffer[MSG_BUFFER_SIZE];
-                    sprintf(out_buffer, "%s", amazonBestsellers[i][k]);
+                    sprintf(out_buffer, "%s", array[i][k]);
+                    //printf("%s\n", out_buffer);
 
                     //send info to client
                     if (mq_send(client_qd, out_buffer, strlen(out_buffer) + 1, 0) == -1) {
@@ -323,7 +353,17 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
         }
     }
 
+
+    char process_array[processes][200][200];
+    //char ***process_array;
+    //process_array = (char ***) calloc(processes, sizeof(char **));
+    //for(int i = 0; i < processes; i++){
+    //    process_array[i] = (char **) calloc(sizes[i], sizeof(char *));
+    //}
+
+
     //sends message to terminate children
+    char output[4000];
     for (int i = 0; i < processes; i++) {
         char client_name[64];
         sprintf(client_name, "/%s", values[i]);
@@ -343,9 +383,156 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
             perror("Parent: mq_send\n");
             exit(1);
         }
-    }
+        
+        //memset(process_str,0,strlen(process_str));
+        int row = 0;
+        while(1){
+            ssize_t count;
+            bzero(output, sizeof(output));
+            do{
+                count = read(fd[0], output, sizeof(output)-1);
+            }while(count <= 0);
+            output[count] = '\0';
 
-    wait(NULL); //ensures all children fully close
+            //size_t length = strlen(output);
+            //process_array[i][row] = malloc(length * sizeof(char));
+            if(strcmp(output, "done") == 0){
+                break;
+            }
+            sprintf(process_array[i][row], output);
+            row++;
+        }
+        //sprintf(process_array[i], process_str);
+        
+    }
+    
+    //printf("%s\n",process_array[0]);
+
+    /*
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            printf("%-40s ", array[i][j]);
+        }
+        printf("\n");
+    }
+    */
+    
+    //PIPE INTERCOMMUNICATION
+    close(fin[1]);
+    close(fout[0]);
+    close(fd[1]);
+    
+    //send message to server when ready
+    write(fout[1], "ready", 10);
+
+    char buffer[20];
+    ssize_t count;
+    char str[4000];
+    while(1){
+        //READ FROM SERVER
+        bzero(buffer, sizeof(buffer));
+        do{
+            count = read(fin[0], buffer, sizeof(buffer)-1);
+        }while(count <= 0);
+        
+        buffer[count] = '\0';
+        
+        //Do option based on choice from client given by server
+        memset(str,0,strlen(str));
+        int option = atoi(buffer);
+        
+
+        if(option == 1){
+            // Do option one
+
+            //Get list of processes
+            for(int i = 0; i < processes; i++){
+                char process_str[100];
+                sprintf(process_str, "%s\n", values[i]);
+                strcat(str, process_str);
+            }
+            //send process list through pipe
+            size_t length = strlen(str);
+            write(fout[1], str, length);
+
+            //read choice from client
+            read(fin[0], buffer, sizeof(buffer));
+
+            //find index of process
+            int choice;
+            for(int i = 0; i< processes; i++){
+                if(strcmp(values[i], buffer) == 0){
+                    choice = i;
+                    break;
+                }
+            }
+            
+            //copy process results into arr
+            char arr[200][200];
+            for(int i = 0; i < 200; i++){
+                strcpy(arr[i], process_array[choice][i]);
+            }
+            
+            //send array to server
+            if(write(fout[1], arr, sizeof(sizeof(char) * 200) * 200) < 0){
+                return 1;
+            }
+            
+        }
+        else if(option == 2){
+            for(int i = 0; i < processes; i++){
+                char process_str[100];
+                sprintf(process_str, "%s\n", values[i]);
+                strcat(str, process_str);
+            }
+
+		size_t length = strlen(str);
+            write(fout[1], str, length);
+
+		read(fin[0], buffer, sizeof(buffer));
+
+
+		 int choice;
+            for(int i = 0; i< processes; i++){
+                if(strcmp(values[i], buffer) == 0){
+                    choice = i;
+                    break;
+                }
+            }
+
+
+	 char arr[200][200];
+            for(int i = 0; i < 200; i++){
+                strcpy(arr[i], process_array[choice][i]);
+            }
+
+
+    	 if(write(fout[1], arr, sizeof(sizeof(char) * 200) * 200) < 0){
+                return 1;
+            }
+	    
+//    	    sprintf(str, "OPTION 2");
+  //          size_t length = strlen(str);
+    //        write(fout[1], str, length);
+        }
+        else if(option == 3){
+            for(int i = 0; i < processes; i++){
+                char process_str[100];
+                sprintf(process_str, "%s : Total books = %d\n", values[i], sizes[i]);
+                strcat(str, process_str);
+            }
+            size_t length = strlen(str);
+            write(fout[1], str, length);
+        }
+        else{
+            break;
+        }
+
+        
+    }
+    
+    //ensures all children fully close
+    wait(NULL); 
 
     //closes server
     if ((mq_close (server_qd)) == -1) {
@@ -362,21 +549,17 @@ int processCreation(int processes, int location, char* values[], int sizes[], in
     /*for (int i = 0; i < processes; i++) {
         printf("Process %d (pid = %d)\n", i, pids[i]);
     }*/
-
     return 0;
 }
 
-
+/*
 int main() {
-
     char input[25];
     printf("Enter a file (bookInfo.txt or amazonBestsellers.txt):\n");
     scanf("%s", input);
-
     if (strcmp(input, "bookInfo.txt") == 0) {
         printf("Which category: Book category, Star rating, or Stock?\n");
         scanf("%s", input);
-
         readFile("bookInfo.txt", 6);
         if (strcmp(input, "Book") == 0) {
             processSetup(703, 6, 1, 43);
@@ -385,11 +568,9 @@ int main() {
         } else if (strcmp(input, "Stock") == 0) {
             processSetup(703, 6, 4, 2);
         } else printf("Incorrect category.\n");
-
     } else if (strcmp(input, "amazonBestsellers.txt") == 0) {
         printf("Which category: User rating, Year, or Genre?\n");
         scanf("%s", input);
-
         readFile("amazonBestsellers.txt", 7);
         if (strcmp(input, "User") == 0) {
             processSetup(550, 7, 2, 10);
@@ -398,12 +579,9 @@ int main() {
         } else if (strcmp(input, "Genre") == 0) {
             processSetup(550, 7, 6, 2);
         } else printf("Incorrect category.\n");
-
     } else {
         printf("Incorrect file name.\n");
     }
-
     return 0;
 }
-
-
+*/
--- server.c
+++ server.c
@@ -6,10 +6,14 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+
+
+#include "process.h"
 
 int main()
 {
-    int port = 5100;
+    int port = 5150;
     int serverSock, clientSock;
     struct sockaddr_in serverAddress, clientAddress;
     socklen_t addrSize;
@@ -46,15 +50,159 @@ int main()
         if (clientSock < 0) {
             exit(1);
         }
+
         printf("Accepted connection from %s:%d\n",inet_ntoa(clientAddress.sin_addr),ntohs(clientAddress.sin_port));
         printf("Number of clients: %d \n\n",++numClients);
+
         if ((childPid = fork()) == 0) {
             close(serverSock);
-            send(clientSock, "hi client", strlen("hi client"), 0);
+
+            //previously use #define MAX 200, in the #include section of the file
+            FILE *filePointer ;
+            char data[100];
+            char options[100][100];
+            filePointer = fopen("options.txt", "r") ; //Opening the Options.txt file
+        
+            if(filePointer == NULL)
+            {
+                    printf("File cant be opened");
+                    exit(0);
+            }
+            else
+            {
+                int i=0;
+                while(fgets(data,100,filePointer)!=NULL) //reading the file line - by - line
+                { 
+                        strcpy(options[i],data); //storing it in the options array. and the first element in the array contains the options that needs to be sent to the client
+                        i++;
+                }
+            }
+
+            printf("Sending options to the client \n");
+            send(clientSock, options[0], sizeof(options[0]), 0);  //sending the options to the client
+            printf("Waiting for the Client feedback \n");
+
+            // read the message from client and copy it in buffer
             bzero(buffer, sizeof(buffer));
             recv(clientSock, buffer, 1024, 0);
-            printf("Response: %s \n", buffer);
+            printf("Response: %s \n\n", buffer);
+            
+            //Create Pipes
+            int fin[2];
+            int fout[2];
+            if(pipe(fin) < 0 || pipe(fout) < 0){
+                perror("pipe");
+                exit(1);
+            }
+            
+            // print buffer which contains the client contents
+            int option = atoi(buffer);
+            if(option == 1){
+
+                char column_names[50] = "Book category,Star rating,Stock";
+
+                write(clientSock,column_names, sizeof(column_names));
+                printf("Waiting for column options\n");
+
+                bzero(buffer, sizeof(buffer));
+                recv(clientSock, buffer, 1024, 0);
+                printf("%s\n",buffer);
+
+                option = atoi(buffer);
+                readFile("bookInfo.txt", 6);
+                if(option == 1)
+                    processSetup(703, 6, 1, 43, fin, fout);
+                else if(option == 2)
+                    processSetup(703, 6, 2, 5, fin, fout);
+                else if(option == 3)
+                    processSetup(703, 6, 4, 2, fin, fout);
+                else
+                    printf("Incorrect category.\n");
+
+            }
+            else if(option == 2){
+                char column_names[50] = "User rating, Year, Genre";
+
+                write(clientSock, column_names, sizeof(column_names));
+                printf("Waiting for column options\n");
+
+                bzero(buffer, sizeof(buffer));
+                recv(clientSock, buffer, 1024, 0);
+                printf("Response: %s\n",buffer);
+                option = atoi(buffer);
+
+                readFile("amazonBestsellers.txt", 7);
+                if(option == 1)
+                    processSetup(550, 7, 2, 10, fin, fout);
+                else if(option == 2)
+                    processSetup(550, 7, 5, 11, fin, fout);
+                else if(option == 3)
+                    processSetup(550, 7, 6, 2, fin, fout);
+                else
+                    printf("Incorrect category.\n");
+
+            }
+            
+            //Close unneccesary pipe ends
+            close(fin[1]);
+            close(fout[0]);
+            
+            //wait for processes to finish
+            char wait_buff[10];
+            read(fin[0], wait_buff, 10);
+            send(clientSock, "Ready", 10, 0);
+
+            char process_buff[4000];
+            //Menu Loop
+            while(1){
+                //recieve choice from user
+                bzero(buffer, sizeof(buffer));
+                memset(buffer, 0 , sizeof(buffer));
+                recv(clientSock, buffer, 1024, 0);
+                int option = atoi(buffer);
+
+                //write choice to process
+                size_t length = strlen( buffer );
+                write( fout[1], buffer, length );
+
+                //read response from process
+                bzero(process_buff, sizeof(process_buff));
+                read(fin[0], process_buff, 4000);
+
+                //send result from process to client
+                send(clientSock, process_buff, sizeof(process_buff), 0);
+
+                if(option == 1){
+                    //wait for process list
+                    bzero(buffer, sizeof(buffer));
+                    recv(clientSock, buffer, 1024, 0);
+
+                    //write client choice to process
+                    length = strlen(buffer);
+                    write(fout[1], buffer, length);
+                    
+                    //read string array from process
+                    char arr[200][200];
+                    if(read(fin[0], arr, sizeof(sizeof(char) * 200) * 200) < 0){
+                        return 1;
+                    }
+
+                    //write string array to client
+                    if(write(clientSock, arr, sizeof(sizeof(char) * 200) * 200) < 0){
+                        return 3;
+                    }
+
+                }
+                else if(option > 3 && option < 1){
+                    break;
+                }
+                
+            }
+            
+        
         }
+        close(clientSock);
+        numClients--;
     }
     // Close the client socket id
     close(clientSock);
